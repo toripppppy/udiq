47522554bc6264ae56bd281e7f4d274fe60aa914
toripppppy <Lundy11@icloud.com>
2024-05-12 15:57:06 +0900

    react to message

--- cogs/udiq_cog.py
+++ cogs/udiq_cog.py
@@ -14,7 +14,21 @@ class UdiqCog(commands.Cog):
 	def __init__(self, bot: commands.Bot):
 		super().__init__()
 		self.bot = bot
+
+		self.knowledges = KnowledgesDAO().find_all()
+
+	@commands.Cog.listener()
+	async def on_message(self, message: discord.Message):
+		# avoid self message
+		if message.author.bot: return
+
+		for kn in self.knowledges:
+			if kn.word in message.content:
+				embed = discord.Embed(title=kn.word, description=kn.meaning)
+				await message.channel.send("おっと、専門用語だよ。", embed=embed)
+				return
 		
+
 	@commands.hybrid_command(name="random", description="random")
 	async def random(self, ctx: commands.Context):
 		knowledges = KnowledgesDAO().find_all()
--- db/daos/knowledges_dao.py
+++ db/daos/knowledges_dao.py
@@ -12,7 +12,7 @@ class KnowledgesDAO:
 	def execute_sql(self, sql: str, params = None) -> None:
 		self.cursor.execute(sql, params)
 
-	def find_one_by_id(self, id: int) -> KnowledgeModel:
+	def find_one_by_id(self, id: int) -> KnowledgeModel | None:
 		self.execute_sql("SELECT * FROM knowledges WHERE id = ?", (id,))
 		result = tuple(*[knowledge for knowledge in self.cursor])
 		
@@ -21,7 +21,7 @@ class KnowledgesDAO:
 		else:
 			return KnowledgeModel(*result)
 		
-	def find_all(self) -> tuple[KnowledgeModel]:
+	def find_all(self) -> tuple[KnowledgeModel] | None:
 		self.execute_sql("SELECT * FROM knowledges")
 		result = tuple([KnowledgeModel(*knowledge) for knowledge in self.cursor])
 
